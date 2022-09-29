7c451b6218f7c19acacdef21df3d7c3962b176ce
Lidiiaa <lidakulagina@gmail.com>
2022-09-29 19:07:40 +0200

    des modifs globales

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    var table = document.getElementById("table");
    
+   /*ajouter des lignes dans le tableau*/ 
+    var table = document.getElementById("table");
     var tableLength = table.rows[1].cells.length;
 
+    function addRow () {
+        //insertion d'une line
+        var row = table.insertRow();
     
-
-function addRow () {
-    //insertion d'une line
-    var row = table.insertRow();
-  
-    //insertion des colons vides
-    
-    for (let i = 0; i < tableLength; i++) {
-        var cell = row.insertCell();
-        cell.innerHTML = "";
+        //insertion des colons vides
         
+        for (let i = 0; i < tableLength; i++) {
+            var cell = row.insertCell();
+            cell.innerHTML = "";
+            
+        }
     }
 
-  }
-
-
-
   addRow();
   addRow();
-
+/*------------*/
 });
+/* --------------- */
 
 
---
+++ scriptLine.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    /*ajout de la classe active on click*/
+    let getStops = document.querySelectorAll('#busStops ul li');
+    getStops.forEach(function(stop) {
+      stop.addEventListener('click', function(e) {
+        getStops.forEach(function(el){
+            el.classList.remove('active');
+        })
+        stop.classList.add('active'); 
+      });
+    });
+});
+
+ 
+ 
+ 
